3d5a10566514a7bee0c512002ae0eef1d1a456a9
Mahmoud <Mahmoud.nasr8@outlook.com>
2026-06-18 21:27:34 +0400

    Initial Portfolio Website

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+});
+
+
+// Welcome message
+
+console.log("Welcome to Mo'men IT Portfolio");
