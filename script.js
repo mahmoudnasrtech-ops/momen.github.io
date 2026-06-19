86f0fc1cbaed159f3c5bc7d361a60fa0ac1d17b7
Mahmoud <Mahmoud.nasr8@outlook.com>
2026-06-19 18:32:58 +0400

    Fixed navbar on scroll

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // Smooth scrolling
-
 document.querySelectorAll('a[href^="#"]').forEach(link => {
-
     link.addEventListener("click", function(e){
-
         e.preventDefault();
-
         document.querySelector(this.getAttribute("href"))
         .scrollIntoView({
-
             behavior:"smooth"
-
         });
-
     });
-
 });
 
-
 // Welcome message
-
 console.log("Welcome to Mo'men IT Portfolio");
+
+// Mobile Hamburger Menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+menuToggle.addEventListener("click", ()=>{
+    navLinks.classList.toggle("active");
+});
