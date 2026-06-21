12087458ce5426a7b023ce1d7a6761d430d0f713
Mahmoud <Mahmoud.nasr8@outlook.com>
2026-06-22 02:43:05 +0400

    Add statistics and back to top button

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ console.log("Welcome to Mo'men IT Portfolio");
 // Mobile Hamburger Menu
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
-menuToggle.addEventListener("click", ()=>{
-    navLinks.classList.toggle("active");
-});
+
+if(menuToggle && navLinks){
+
+    menuToggle.addEventListener("click", ()=>{
+        navLinks.classList.toggle("active");
+    });
+
+}
+
